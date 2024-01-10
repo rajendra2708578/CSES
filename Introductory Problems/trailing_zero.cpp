602ecef44f93aee94b30a288a35b57cf0d717fa8
rajendra <rajendranathmurmu0827@gmail.com>
2024-01-10 23:37:51 +0530

    two more code

---
+++ Introductory Problems/trailing_zero.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f first
+#define s second
+#define pb push_back
+int const mod=1e9+7;
+// ll power(ll pi,ll n)
+// {
+//     ll ans=1;
+//     while(n)
+//     {
+//         if(n%2==0)
+//         {
+//             pi=(pi*pi)%mod;
+//             n/=2;
+//         }
+//         else
+//         {
+//             ans=(ans*pi)%mod;
+//             n-=1;
+//         }
+//     }
+//     return ans%mod;
+// }
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll count=0;
+    ll value=5;
+    while(value<=n)
+    {
+        count+=(n)/value;
+        value*=5;
+    }
+    cout<<count<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // ll t;
+    // cin>>t;
+    // while(t--)
+    // {
+        solve();
+    // }
+    return 0;
+}
+
