99f297575deb93a82c7daec4d914e42f7942a922
rajen <nathrajen2710@gmail.com>
2024-02-12 23:37:50 +0530

    more added

---
+++ Sorting and Searching/Subarray Distinct Values.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f first
+#define s second
+#define pb push_back
+int const mod=1e9+7;
+// power function  --------------------------------//
+ll power(ll x, ll y, ll p =mod)
+{
+    unsigned long long res = 1;
+
+
+    x = x % p;
+    while (y > 0)
+    {
+
+
+        if (y & 1)
+            res = (res * x) % p;
+
+
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+// Mod inverse using fermat's little ------------------
+ll modInverse(ll n,ll p=mod)
+{
+    return power(n,p-2,p);
+}
+// Combination formula---------------------------------
+ll nCr(ll n, ll r, ll p=mod)    
+{
+    if (n < r)
+        return 0;
+
+
+    if (r == 0)
+        return 1;
+       
+    vector<ll> fac(n+1,0);
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+
+
+    return (fac[n] * modInverse(fac[r], p) % p * modInverse(fac[n - r], p) % p) % p;
+}
+// end of pre compute functions---------//
+
+
+
+
+
+
+
+void solve()
+{
+  ll n,k;
+  cin>>n>>k;
+  ll arr[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+  unordered_map<ll,ll>mp;
+  ll i=0,j=0,count=0,no=0;;
+  while(j<n)
+  {
+        mp[arr[j]]++;
+        while(mp.size()>k && i<j)
+        {
+            mp[arr[i]]--;
+            if(mp[arr[i]]==0)
+            mp.erase(arr[i]);
+            i++;
+        }
+       count+=(j-i)+1;
+       j++;
+  }
+//   j-=1;
+//   while(i+1<j)
+//   {
+    
+//     mp[arr[i]]--;
+//     if(mp[arr[i]]==0)
+//     mp.erase(arr[i]);
+//     i++;
+//     if(mp.size()<=k && i+1<j)
+//     {
+//         // cout<<i<<" "<<j<<" "<<mp.size()<<endl;
+//         count++;
+//     }
+//   }
+  cout<<count<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // ll t;
+    // cin>>t;
+    // while(t--)
+    // {
+        solve();
+    // }
+    return 0;
+}
+
+
+
+
+
